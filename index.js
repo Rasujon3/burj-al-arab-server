0d2742d3f163215f14582407c4ca092cebf7b0a3
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-09-14 20:20:44 +0600

    booking information collect & save in database
    video : 3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 const express = require('express')
 const app = express()
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const port = 5000
+app.use(cors());
+app.use(bodyParser.json());
 
 const pass = "ArabianHorse79";
 
@@ -10,10 +16,16 @@ const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://arabian:ArabianHorse79@cluster0.3l9g8.mongodb.net/burjAlArab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("burjAlArab").collection("bookings");
+    const bookings = client.db("burjAlArab").collection("bookings");
     // perform actions on the collection object
-    console.log('db connected successfully')
-    client.close();
+    app.post('/addBooking',(req,res)=>{
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result =>{
+                res.send(result.insertedCount > 0);
+            })
+        console.log(newBooking);
+    })
 });
 
 
